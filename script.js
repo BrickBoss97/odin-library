465dd32269c7c690775f1c8ec3af0faa8b950843
BrickBoss97 <unknownyoungster@gmail.com>
2024-05-28 07:41:34 -0700

    Link form to object function. Add new book creation DOM function.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const dialog = document.querySelector("dialog");
+const form = document.querySelector("#myForm")
+const bookContainer = document.querySelector(".books")
 
 function openForm() {
     dialog.showModal();
@@ -10,13 +12,51 @@ function closeForm() {
 
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = 'not read yet';
+    this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    const titleInput = document.querySelector(".titleInput").value;
+    const authorInput = document.querySelector(".authorInput").value;
+    const pagesInput = document.querySelector(".pagesInput").value;
+    const readInput = document.querySelector(".readInput").value;
+
+    myLibrary.push(new Book(titleInput, authorInput, pagesInput, readInput));
+    
+    form.reset();
+
+    displayNewBook()
 }
+
+function displayNewBook() {
+    let newBook = myLibrary.at(-1);
+
+    const divBook = document.createElement("div");
+    divBook.classList.add("book");
+    bookContainer.append(divBook);
+
+    const bookTitle = document.createElement("h2");
+    bookTitle.classList.add("title");
+    bookTitle.textContent = newBook.title;
+    divBook.append(bookTitle);
+    
+    const bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("author");
+    bookAuthor.textContent = newBook.author;
+    divBook.append(bookAuthor);
+    
+    const bookPages = document.createElement("div");
+    bookPages.classList.add("pages");
+    bookPages.textContent = `Pages: ${newBook.pages}`;
+    divBook.append(bookPages)
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    dialog.close();
+});
